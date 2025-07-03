4506446e765b514fd26d1c1166146c776a4b6e12
isaasz18 <isabeli.gaspar@escola.pr.gov.br>
2025-07-03 13:31:25 +0000

    aula5

--- main.js
+++ main.js
@@ -7,16 +7,34 @@ const botoes = document.querySelectorAll ('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha --;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-function aumentaTamanho(){
-    if (tamanhoSenha < 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha ++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-semha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i <tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = 
+
 }
